f0ad11a1be7b5d8ce81204e8b10b012d445cff26
Sakib Rasul <work@sakibrasul.com>
2023-08-18 11:38:53 -0400

    init

---
+++ src/routes/teas.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import Form from "../form";
+
+// This component will render in the root's outlet at `/teas`.
+export default function Teas() {
+    // Access this route's loader data and store it in state.
+    const [ teas, setTeas ] = useState(useLoaderData());
+
+    // We'll pass this function to <Form> to handle its `submit` event.
+    function handleSubmit(event) {
+        // Prevent default behavior.
+        event.preventDefault();
+        // POST the newly submitted tea and update state.
+        fetch("http://localhost:3000/teas", {
+            method: "POST",
+            headers: { "Content-Type": "application/json", "Accept": "application/json" },
+            body: JSON.stringify({
+                id: teas.length + 1,
+                name: event.target.name.value,
+                price: parseInt(event.target.price.value)
+            })
+        }).then(response => response.json())
+          .then(newTea => setTeas([...teas, newTea]));
+    }
+
+    // Render a grid of teas, as well as a form for adding new ones.
+    return (
+        <div className="mt-2.5 grid grid-cols-2 gap-5">
+            {teas.map(tea => 
+                <div key={tea.id}>
+                    <p>{tea.name}</p>
+                    <sub>${tea.price}.00</sub>
+                </div>
+            )}
+            <Form handleSubmit={handleSubmit} />
+        </div>
+    )
+}
